460aa469c0b15c201f6839e9de0988d246fafad3
HEGGIX <glebermakovich@gmail.com>
2025-01-13 21:33:44 +0300

    edit api

--- src/api/telegramApi/index.ts
+++ src/api/telegramApi/index.ts
@@ -1,7 +1,7 @@
-const baseUrl = "https://api.telegram.org/bot8141493584:AAF5ei0aStfV60FBSc_3jkAICc1EucohI28/"
+const baseUrl = "https://api.telegram.org/bot7812137498:AAFS03d24rMqVBXS_KlAneYBOTMSrd12smE/"
 
 const sendMessage = async (message:string): Promise<void> => {
-    const url = `${baseUrl}sendMessage?chat_id=-4664286917&text=${message}`
+    const url = `${baseUrl}sendMessage?chat_id=-4748933216&text=${message}`
 
     const response = await fetch(url)
 
