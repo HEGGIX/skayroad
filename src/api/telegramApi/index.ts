b06aa88c5c14889ce4ce14ad0c9867d959b57128
Dmitry Kuchynski <dmitry.kuchynski@cortlex.com>
2025-05-16 10:41:07 +0300

    fix form 400 error

--- src/api/telegramApi/index.ts
+++ src/api/telegramApi/index.ts
@@ -1,13 +1,14 @@
 const baseUrl = "https://api.telegram.org/bot7812137498:AAFS03d24rMqVBXS_KlAneYBOTMSrd12smE/"
 
 const sendMessage = async (message:string): Promise<void> => {
-    const url = `${baseUrl}sendMessage?chat_id=-4748933216&text=${message}`
+    const encodedMessage = encodeURIComponent(message)
+    const url = `${baseUrl}sendMessage?chat_id=-1002501606692&text=${encodedMessage}`
 
     const response = await fetch(url)
 
     if(!response.ok){
         const error = await response.json()
-        await Promise.reject(error.description || "Что-то пошло не так")
+        return Promise.reject(error.description || "Что-то пошло не так")
     }
 }
 export default sendMessage
