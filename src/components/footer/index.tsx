e095fe4fa4f8cfe235197cf480fc72e59c5cbccf
Dmitry Kuchynski <dmitry.kuchynski@cortlex.com>
2025-07-18 10:30:12 +0300

    changes for unp

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -7,15 +7,16 @@ const Footer = () => {
             <div className="footer-wrapper">
                 <div className="footer-column first">
                     <span className="footer-column__text">© {new Date().getFullYear()} – Все права защищены</span>
-                    <span className="footer-column__text"><br/>Адрес: Беларусь, г. Минск,<br/>Берута 3Б, 415 каб.</span>
+                    <span
+                        className="footer-column__text"><br/>Адрес: Беларусь, г. Минск,<br/>Некрасова 114, Оф. 84.<br/>Пом.(кабинет 1-40), 220068</span>
                 </div>
                 <div className="footer-column">
                     <span className="footer-column__text">График работы офиса:</span>
                     <span className="footer-column__text"><br/>ПН - ПТ: 09:00 - 17:00 <br/> СБ, ВС - Выходной</span>
                 </div>
                 <div className="footer-column third">
-                    <span className="footer-column__text">ООО "СКАЙРОУД", УНП: 193797907 <br/> Карт-счет: </span>
-                    <span className="footer-column__text">BY85ALFA30122F47810010270000</span>
+                    <span className="footer-column__text">ООО "ХЕППАКОМПАНИ", УНП: 193872830 <br/> Карт-счет: </span>
+                    <span className="footer-column__text">BY08ALFA30122H00660010270000</span>
                     <span className="footer-column__text"><br/>в BYN в ЗАО 'Альфа-Банк', БИК: ALFABY2X</span>
                 </div>
             </div>
